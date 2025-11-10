1134bcd812be381ef95299dc45c29af5eb62c799
ido karpel <idokarpel@idos-MacBook-Pro.local>
2025-11-10 13:12:33 +0200

    תיאור קצר וברור של המשימה או השינויים שבוצעו

--- Cheers.java
+++ Cheers.java
@@ -3,11 +3,21 @@
 /*
 Great work boaz!
 */
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	     String word = args[0];
+             int num = Integer.parseInt(args[1]); 
+             for (int i = 0; i < word.length(); i++) {
+                char currentChar = word.charAt(i);
+                              
+                    System.out.println("Give me an "+ currentChar+ ":"+ currentChar+"!"); 
+        
+            } 
+            System.out.println("What does that spell?");
+            for ( int j = 0; j < num; j++) {
+                System.out.println( word+"!!!");
+            }
+                 
         }
 }
