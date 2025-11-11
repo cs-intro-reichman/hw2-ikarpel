e60cbdf505d3994944ad4334e5813e6023665b5f
ido karpel <idokarpel@idos-MacBook-Pro.local>
2025-11-11 12:05:26 +0200

    הודעת תיאור ברורה על השינוי

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int k = Integer.parseInt(args[0]);
+		double sum = 0;
+		double div = 0;
+		for (int i = 0; i < k; i++) {
+			if (i % 2 == 0) {
+				sum += 1.0/div;
+			} else {
+				sum -= 1.0/div;
+			}
+			div += 2.0;
+		}
+		System.out.println("pi according to java: "+ Math.PI);
+		System.out.println("pi, approximated: "+ (sum * 4.0));
 	}
 }
